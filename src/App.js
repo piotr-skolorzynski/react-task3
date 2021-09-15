bf330f3b405908a3d426d87e43900f5945f80879
Piotr <p.skolorzynski@gmail.com>
2021-09-15 13:49:19 +0200

    create Wrapper component to style page 	modify index.css add style for Wrapper component 	install bootstrap and react-bootstrap, modify index.js and package.json files 	modify App component fetch data from json-placeholder render photos on page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,60 @@
-
+import { useState, useEffect } from 'react';
+import Wrapper from './Wrapper';
+import Table from 'react-bootstrap/Table';
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('https://jsonplaceholder.typicode.com/photos')
+      .then(res => res.json())
+      .then(data => data.filter(item => item.albumId <= 5))
+      .then(filteredAlbums => {
+        setIsLoading(false);
+        // console.log(filteredAlbums);
+        setPhotos(filteredAlbums);
+      });
+  }, []);
+
+  const handleClick = e => {
+    console.log(e.target.closest('tr'))
+  }
+
   return (
-    <h2>React-Task3</h2>
+    <>
+    <Wrapper>
+    <Table borderless variant="dark" className="table-wrapper"> 
+      <thead >
+        <tr>
+          <th>Title</th>
+          <th>Thumbnail</th>
+        </tr>
+      </thead>
+      <tbody>
+            {isLoading && 
+                <tr>
+                  <td>Loading...</td>
+                  <td></td>
+                </tr>}
+            {photos && photos.map(photo => {
+              const {id, title, thumbnailUrl} = photo;
+              return (
+                <>
+                <tr key={id}>
+                  <td className="photo-title" onClick={e => handleClick(e)}>{title}</td>
+                  <td>
+                    <img src={thumbnailUrl} alt={title} />
+                  </td>
+                </tr>
+                </>
+                )
+            })}
+      </tbody>
+    </Table>
+    </Wrapper>
+    </>
   );
 }
 
---
+++ src/Wrapper.js
@@ -0,0 +1,9 @@
+const Wrapper = ({children}) => {
+    return (
+        <main className="app-container">
+            {children}
+        </main>
+    );
+}
+ 
+export default Wrapper;
